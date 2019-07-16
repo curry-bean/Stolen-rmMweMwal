29f5d366400f7d51a4d69c81262b610ceb604880
Hugo Bruvry <hugo.bruvry@gmail.com>
2019-07-16 13:19:46 +0200

    Change demoreel at reload

--- js/interact.js
+++ js/interact.js
@@ -137,7 +137,7 @@ function onCloseAreaClick(e)
         interactiveArea.style.opacity = "0";
 
         setTimeout(function(){
-            demoreel.innerHTML = "<iframe src=\"https://player.vimeo.com/video/236240897?color=FFFFFF&title=0&byline=0&portrait=0\" frameborder=\"0\" allow=\"autoplay; fullscreen\" allowfullscreen></iframe>";
+            demoreel.innerHTML = "<iframe src=\"https://player.vimeo.com/video/348352868?color=FFFFFF&title=0&byline=0&portrait=0\" frameborder=\"0\" allow=\"autoplay; fullscreen\" allowfullscreen></iframe>";
         }, 50);
     }
 
